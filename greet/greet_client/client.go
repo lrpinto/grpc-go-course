37721ff71a977970a309211fcdf6e4c746f1cdaf
Luisa <luisa.rebelopinto@gmail.com>
2020-12-29 16:46:22 +0000

    Unary API Client Implementation

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/lrpinto/grpc-go-course/greet/greetpb"
 	"google.golang.org/grpc"
@@ -8,7 +9,7 @@ import (
 )
 
 func main() {
-	fmt.Print("Hello I am a client!")
+	fmt.Print("Hello I am a client!\n")
 
 	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
@@ -18,5 +19,26 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	log.Printf("Created client: %f", c)
+	//log.Printf("Created client: %f", c)
+
+	doUnary(c)
+
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do Unary RPC...")
+
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Luisa",
+			LastName:  "Pinto",
+		},
+	}
+
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling Greet RPC: %v", err)
+	}
+
+	log.Printf("Response from Greet: %v", res.Result)
 }
