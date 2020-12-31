d9bd2c50192e02c9d3f7fe710a837586830dc6a0
Luisa <luisa.rebelopinto@gmail.com>
2020-12-31 12:50:27 +0000

    ComputeAverage Client Implementation (Client Streaming)

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -23,9 +23,55 @@ func main() {
 	// create calculator client
 	c := calculatorpb.NewCalculatorServiceClient(cc)
 
+	// Do Unary RPC
 	doUnary(c)
 
+	// Do Server Streaming RPC
 	doServerStreaming(c)
+
+	// Do Client Streaming RPC
+	doClientStreaming(c)
+}
+
+func doClientStreaming(c calculatorpb.CalculatorServiceClient) {
+	log.Printf("Starting to do ComputeAverage Client Streaming RPC...\n")
+
+	ctx := context.Background()
+
+	requests := []*calculatorpb.ComputeAverageRequest{
+		{
+			Parcel: 1,
+		},
+		{
+			Parcel: 2,
+		},
+		{
+			Parcel: 3,
+		},
+		{
+			Parcel: 4,
+		},
+	}
+
+	averageClient, err := c.ComputeAverage(ctx)
+	if err != nil {
+		log.Fatalf("Error while calling ComputeAverage\n")
+	} else {
+		for _, req := range requests {
+			err := averageClient.Send(req)
+			if err != nil {
+				log.Fatalf("Error while sending request: %v\n", req)
+			} else {
+				log.Printf("Sent request: %v\n", req)
+			}
+		}
+		res, err := averageClient.CloseAndRecv()
+		if err != nil {
+			log.Fatalf("Error while receiving response: %v", res)
+		} else {
+			log.Printf("Average: %v", res.GetAverage())
+		}
+	}
 }
 
 func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
